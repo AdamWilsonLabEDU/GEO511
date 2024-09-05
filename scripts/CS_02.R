e713ae2ae9d49bf679a6aa5e1c8913247358f8ce
Adam M. Wilson <900623+adammwilson@users.noreply.github.com>
2024-09-05 13:06:56 -0400

    update ps2

--- scripts/CS_02.R
+++ scripts/CS_02.R
@@ -42,7 +42,7 @@ md_bullet(rmarkdown::metadata$reading)
 library(tidyverse)
 
 # define the link to the data - you can try this in your browser too.  Note that the URL ends in .txt.
-dataurl="https://data.giss.nasa.gov/tmp/gistemp/STATIONS/tmp_USW00014733_14_0_1/station.txt"
+dataurl="https://data.giss.nasa.gov/tmp/gistemp/STATIONS_v4/tmp_USW00014733_14_0_1/station.csv"
 
 #' 
 #' 
@@ -52,9 +52,9 @@ dataurl="https://data.giss.nasa.gov/tmp/gistemp/STATIONS/tmp_USW00014733_14_0_1/
 #the next line tells the NASA site to create the temporary file
 httr::GET("https://data.giss.nasa.gov/cgi-bin/gistemp/stdata_show_v4.cgi?id=USW00014733&ds=14&dt=1")
 # the next lines download the data
-temp=read_table(dataurl,
-              skip=3, #skip the first line which has column names
+temp=read_csv(dataurl, 
               na="999.90", # tell R that 999.90 means missing in this dataset
+              skip=1, # we will use our own column names as below so we'll skip the first row
               col_names = c("YEAR","JAN","FEB","MAR", # define column names 
                             "APR","MAY","JUN","JUL",  
                             "AUG","SEP","OCT","NOV",  
